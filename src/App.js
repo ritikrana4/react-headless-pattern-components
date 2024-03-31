3f8b10e6c04089069f9f8135012ea86ad219814e
Ritik Rana <ritikrana4@gmail.com>
2024-03-31 10:01:52 +0000

    changes

--- src/App.js
+++ src/App.js
@@ -2,8 +2,13 @@ import React from "react";
 import Accordion from "./components/Accordion/Accordion";
 
 export default function App() {
+  const appStyle= {
+    display:"flex",
+    width:"100vw",
+    justifyContent:"center",
+  }
   return (
-    <div>
+    <div style={appStyle}>
       <Accordion />
     </div>
   );
